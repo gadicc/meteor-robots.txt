3ee590085638cfacab0b97992e3cca3835764f01
Gadi Cohen <dragon@wastelands.net>
2013-08-28 18:25:54 +0300

    fix 0.6.4 and below

--- package.js
+++ package.js
@@ -3,7 +3,15 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-	api.use('standard-app-packages', 'server');
+	// TODO, support 0.6.4 and below until 2014
+	try {
+	    api.use('webapp', 'server');
+	}
+	catch (error) {
+	    if (error.code != 'ENOENT')
+	        throw(error);
+	}	
+
 	api.add_files('robots.js', 'server');
 	if (api.export)
 		api.export('robots', 'server');
