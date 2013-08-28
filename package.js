368cfa43a39e57639c9250939235fb6283b96146
Gadi Cohen <dragon@wastelands.net>
2013-08-28 18:14:02 +0300

    0.6.5 support

--- package.js
+++ package.js
@@ -3,5 +3,6 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+	api.use('standard-app-packages', 'server');
 	api.add_files('robots.js', 'server');
 });
--- robots.js
+++ robots.js
@@ -1,8 +1,21 @@
+var root, match, stack;
 var fs = Npm.require('fs');
-var root = __meteor_bootstrap__.bundle.root;
+
+if (__meteor_bootstrap__.bundle) {
+	// TODO, remove in 2014
+	root = __meteor_bootstrap__.bundle.root;
+} else {
+	// TODO, see how Meteor does this in 0.6.5+
+	match = /^(.*)\/\.meteor\/.*/.exec(__meteor_bootstrap__.serverDir);
+	root = match[1];
+}
+
+// TODO, remove in 2014
+stack = __meteor_bootstrap__.app ? __meteor_bootstrap__.app.stack
+      : WebApp.connectHandlers.stack;
 
 // splice necessary to jump ahead of original /robots.txt route
-__meteor_bootstrap__.app.stack.splice(0, 0, {
+stack.splice(0, 0, {
 	route: '/robots.txt',
 	handle: function(req, res, next) {
 		fs.readFile(root + '/static/robots.txt', function(err, data) {
@@ -15,7 +28,6 @@ robots = {
 	lines: []
 };
 
-console.log(__meteor_bootstrap__.bundle.root);
 function robotsOut(res, err, data) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 
